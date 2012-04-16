6b1b676da18f6356fbc74e05266859b01b23d401
James Harton <jamesotron@gmail.com>
2012-04-17 11:08:57 +1200

    Empty gem

---
+++ lib/ember-data-faye-adapter.rb
@@ -0,0 +1,11 @@
+require "ember-data-faye-adapter/version"
+
+module Ember
+  module Data
+    module Faye
+      module Adapter
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/ember-data-faye-adapter/version.rb
@@ -0,0 +1,9 @@
+module Ember
+  module Data
+    module Faye
+      module Adapter
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
